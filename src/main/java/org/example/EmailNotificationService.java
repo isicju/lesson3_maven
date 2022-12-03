d6f903a4d58feaf4635440a4dd5e29255be2a9ff
icju <isicju@gmail.com>
2022-12-03 16:07:57 +0000

    sms  service

--- src/main/java/org/example/EmailNotificationService.java
+++ src/main/java/org/example/EmailNotificationService.java
@@ -8,19 +8,33 @@ import com.mailjet.client.resource.Emailv31;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.nio.charset.Charset;
+
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.client.RestTemplate;
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.Charset;
 
 public class EmailNotificationService {
 
-    public static void main(String[] args) {
-        String name = "";
-        String password = "";
+    public static void main1(String[] args) {
+        String name = "53ffc361c9d9692e9060ef89c18f1c11";
+        String password = "47db15682b550bdf44e2609e94c0dd4a";
+
         String recipient = "isicju@gmail.com";
-        String textMessage = "bla";
+        String textMessage = "kekekek";
 
         ClientOptions options = ClientOptions.builder()
                 .apiKey(name)
                 .apiSecretKey(password)
                 .build();
+
         MailjetClient client = new MailjetClient(options);
         MailjetRequest request = new MailjetRequest(Emailv31.resource)
                 .property(Emailv31.MESSAGES, new JSONArray()
@@ -39,6 +53,28 @@ public class EmailNotificationService {
         } catch (MailjetException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static void main(String[] args) {
+        String username = "isicju@gmail.com";
+        String password = "MjHqfCWBKh63W12Ognw5aM4XzR";
+
+        String phone = "79681907742";
+        String message = "testfromjavacourse";
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders() {{
+            String auth = username + ":" + password;
+            byte[] encodedAuth = Base64.encodeBase64(
+                    auth.getBytes(Charset.forName("US-ASCII")));
+            set("Authorization", "Basic " + new String(encodedAuth));
+            set("Content-Type", "application/json");
+        }};
+
+
+        String body = "{\"number\":" + phone + ", \"text\":\"" + message + "\",\"sign\":\"SMS Aero\"}";
+        HttpEntity<String> request = new HttpEntity<>(body, headers);
+        restTemplate.postForObject("https://gate.smsaero.ru/v2/sms/send", request, String.class);
 
     }
 
