3f272058985cc37b1f7fd98e7e4d4eb5a87b61ff
icju <isicju@gmail.com>
2022-12-03 15:25:29 +0000

    initial commit, simple maven based project

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("works");
+
+    }
+
+    public String helloWorld(){
+        return "hello world";
+    }
+
+}
