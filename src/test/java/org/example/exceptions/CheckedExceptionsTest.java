5a6132844309626a8f6260721e1afcabdeeef356
icju <isicju@gmail.com>
2022-12-18 13:08:59 +0000

    cleanup

---
+++ src/test/java/org/example/exceptions/CheckedExceptionsTest.java
@@ -0,0 +1,13 @@
+package org.example.exceptions;
+
+import org.example.DefaultTest;
+import org.junit.jupiter.api.Test;
+
+public class CheckedExceptionsTest extends DefaultTest {
+
+    @Test
+    public void throwsFileNotFoundTest() {
+
+    }
+
+}
