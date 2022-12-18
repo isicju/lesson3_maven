5779d56b3decdf283a07f77508d20e368f170990
icju <isicju@gmail.com>
2022-12-18 17:20:22 +0000

    MajorProblem type fix

---
+++ src/main/java/org/example/algo/MajorProblem.java
@@ -0,0 +1,14 @@
+package org.example.algo;
+
+public class MajorProblem {
+//Given an integer array of size `n`, return the element
+// which appears more than `n/2` times.
+// Assume that the input always contain the majority element.
+
+// if target elemnt is not present - throw an exception (cover that case in tests)
+
+    private int findMajor(String[] input, int target){
+        return 0;
+    }
+
+}
