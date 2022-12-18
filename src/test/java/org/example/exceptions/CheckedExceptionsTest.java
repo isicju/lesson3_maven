f803e17a7938bdcd232108a019620ae2f1a1258e
icju <isicju@gmail.com>
2022-12-18 13:08:59 +0000

    cleanup

--- src/test/java/org/example/exceptions/CheckedExceptionsTest.java
+++ src/test/java/org/example/exceptions/CheckedExceptionsTest.java
@@ -3,8 +3,6 @@ package org.example.exceptions;
 import org.example.DefaultTest;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileNotFoundException;
-
 public class CheckedExceptionsTest extends DefaultTest {
 
     @Test
