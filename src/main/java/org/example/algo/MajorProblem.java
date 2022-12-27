6c14bf3b2a9344bffd5168608b3c05530a260256
Nikita Fonov <fononita@gmail.com>
2022-12-27 18:27:48 +0100

    MajorProblemTest same trouble

--- src/main/java/org/example/algo/MajorProblem.java
+++ src/main/java/org/example/algo/MajorProblem.java
@@ -10,25 +10,29 @@ public class MajorProblem {
 //Input : [1, 3, 1, 1]
 //Output: 1
 
-    public static int findMajorityElement(int[] input){
-        Arrays.sort(input);
-        int currentMaxValue = input[0];
-        int currentMaxCounter = 1;
-        int potentialMaxCounter = 1;
-        for (int i = 0; i < input.length - 1; i++) {
-            if (input[i] == input[i+1]) {
-                potentialMaxCounter++;
-            }
-            else {
-                if (currentMaxCounter < potentialMaxCounter) {
-                    currentMaxCounter = potentialMaxCounter;
-                    potentialMaxCounter = 1;
-                    currentMaxValue = input[i];
+    public static int findMajorityElement(int[] input) {
+        int currentMaxValue = 0;
+        try {
+            Arrays.sort(input);
+            currentMaxValue = input[0];
+            int currentMaxValueCounter = 1;
+            int potentialMaxValueCounter = 1;
+            currentMaxValue = 1;
+
+            for (int i = 0; i < input.length - 1; i++) {
+                if (input[i] == input[i + 1]) {
+                    potentialMaxValueCounter++;
+                } else {
+                    if (currentMaxValueCounter < potentialMaxValueCounter) {
+                        currentMaxValueCounter = potentialMaxValueCounter;
+                        potentialMaxValueCounter = 1;
+                        currentMaxValue = input[i];
+                    } else potentialMaxValueCounter = 1;
                 }
-                else potentialMaxCounter = 1;
             }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array is null!");
         }
         return currentMaxValue;
     }
-
 }
--- src/test/java/org/example/algo/MajorProblemTest.java
+++ src/test/java/org/example/algo/MajorProblemTest.java
@@ -1,9 +1,31 @@
 package org.example.algo;
 
 import org.example.DefaultTest;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MajorProblemTest extends DefaultTest {
 
+    @Test
+    public void oneRightAnswerTest() {
+        int[] input = new int[]{1, 1, 2, 4, 5};
+        int rightOutput = 1;
+        assertEquals(MajorProblem.findMajorityElement(input), rightOutput);
+    }
+    @Test
+    public void twoRightAnswersTest() {
+        int[] input = new int[]{1, 1, 2, 2, 4, 5};
+        int rightOutput = 1;
+        assertEquals(MajorProblem.findMajorityElement(input), rightOutput);
+    }
+    @Test
+    public void nullInArrayTest() {
+        int[] input = new int[0];
+        assertThrows(ArrayIndexOutOfBoundsException.class,
+                () -> MajorProblem.findMajorityElement(input),
+                "Had to trow an exception but it didn't");
+    }
     //cover all main scenarios
 
 }
