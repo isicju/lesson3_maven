adccae4127811d048d8decd510e78cb0d00abda4
Nikita Fonov <fononita@gmail.com>
2022-12-27 19:46:47 +0100

    TaxServiceTest 100% coverage done

--- src/test/java/org/example/taxcalc/TaxServiceTest.java
+++ src/test/java/org/example/taxcalc/TaxServiceTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TaxServiceTest extends DefaultTest {
 
     TaxService taxService;
@@ -23,4 +25,45 @@ public class TaxServiceTest extends DefaultTest {
         Assertions.assertEquals(valueAfterTax, 100860.0f);
     }
 
+    @DisplayName("Verifying null user")
+    @Test
+    public void nullUserTest(){
+        Assertions.assertThrows(UserValidationException.class,
+                () -> taxService.calculateYearIncome(null),
+                "Had to trow an exception but it didn't");
+    }
+
+    @DisplayName("Verifying null region")
+    @Test
+    public void nullRegionTest(){
+        User invalidUserFromUS = new User(null, "John Smith",  123000, 12);
+        Assertions.assertThrows(UserValidationException.class,
+                () -> taxService.calculateYearIncome(invalidUserFromUS),
+                "Had to trow an exception but it didn't");
+    }
+
+    @DisplayName("Verifying user FullName getter")
+    @Test
+    public void userFullNameGetterTest(){
+        User validUserFromUS = new User(Region.US, "John Smith",  123000, 12);
+        assertEquals("John Smith", validUserFromUS.getFullName());
+    }
+
+    @DisplayName("Verifying user getter")
+    @Test
+    public void userGetterTest(){
+        User validUserFromUS = new User(Region.US, "John Smith",  123000, 12);
+        UserValidationException exception = new UserValidationException("", validUserFromUS);
+        assertEquals(exception.getUser(), validUserFromUS);
+    }
+
+    @DisplayName("Verifying user getter")
+    @Test
+    public void userSetterTest(){
+        User first = new User(Region.US, "John Smith",  123000, 12);
+        User second = new User(Region.US, "Mister Andersen",  481516, 1);
+        UserValidationException exception = new UserValidationException("", first);
+        exception.setUser(second);
+        assertEquals(exception.getUser(), second);
+    }
 }
