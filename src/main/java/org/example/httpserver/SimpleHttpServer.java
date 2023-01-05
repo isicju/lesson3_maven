3d6d2a7dd3a204048a705021f18aaec169c53b1f
ramru <ramrush-r@mail.ru>
2023-01-05 20:36:12 +0300

    fixup! + CityMapApi handler

--- src/main/java/org/example/httpserver/SimpleHttpServer.java
+++ src/main/java/org/example/httpserver/SimpleHttpServer.java
@@ -1,21 +1,15 @@
 package org.example.httpserver;
 
 import com.google.gson.Gson;
-import com.pengrad.telegrambot.model.File;
-import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
-import org.apache.commons.io.IOUtils;
 import org.example.lesson7.api.UserApi;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.net.URI;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.Objects;
 
 import static org.example.utils.DataUtil.getFileAsByteArray;
 
@@ -25,28 +19,11 @@ public class SimpleHttpServer {
 
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8500), 0);
-        // Here is UserAPi handlers. Currently, unavailable
-        //HttpContext context = server.createContext("/");
-        //context.setHandler(SimpleHttpServer::handleRequest);
-
-        HttpContext cityApiContext = server.createContext("/");
-        cityApiContext.setHandler(SimpleHttpServer::handleCityApiRequest);
-
+        HttpContext context = server.createContext("/");
+        context.setHandler(SimpleHttpServer::handleRequest);
         HttpContext cityContext = server.createContext("/cities");
         cityContext.setHandler(SimpleHttpServer::handleCityRequest);
         server.start();
-    }
-
-    private static void handleCityApiRequest(HttpExchange exchange) throws IOException {
-
-        byte[] indexHtmlPage = getFileAsByteArray("....../index.html");
-
-        Headers h = exchange.getResponseHeaders();
-        h.set("Content-Type","text/html");
-        OutputStream os = exchange.getResponseBody();
-        exchange.sendResponseHeaders(200,512);
-        os.write(Objects.requireNonNull(indexHtmlPage));
-        os.close();
     }
 
     private static void handleRequest(HttpExchange exchange) throws IOException {
