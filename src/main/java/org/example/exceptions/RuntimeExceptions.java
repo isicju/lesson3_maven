62d7c4e201c8152b5f01b5bbf88a90da06128bad
exenoyon <exenoyon@yandex.ru>
2023-01-07 00:29:56 +0800

    add RuntimeExceptions

--- src/main/java/org/example/exceptions/RuntimeExceptions.java
+++ src/main/java/org/example/exceptions/RuntimeExceptions.java
@@ -1,5 +1,7 @@
 package org.example.exceptions;
 
+import org.example.online.Client;
+
 public class RuntimeExceptions {
 
     public void throwsNullPointerException(){
@@ -8,19 +10,32 @@ public class RuntimeExceptions {
     }
 
     public void throwsArrayIndexOutOfBoundsException(){
-
+        int[] arr = {1};
+        System.out.println(arr[1]);
     }
 
     public void throwsNumberFormatException(){
-
+        String s = "4o4";
+        Integer number = Integer.parseInt(s);
+        System.out.println(number);
     }
 
     public void throwsIllegalArgumentException(){
-
+        Client client = null;
+       SomeMethod(client);
     }
 
     public static void throwsClassCastException(){
-
+        Integer x = 100;
+        Object object = x;
+        SomeMethod(object);
     }
 
+    private static void SomeMethod(Object arg)
+    {
+        if(arg == null)
+            throw new IllegalArgumentException();
+
+        System.out.println((String) arg);
+    }
 }
