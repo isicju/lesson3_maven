4011df41357e9dd6ab2d66f05b13a9514b29caf9
exenoyon <exenoyon@yandex.ru>
2023-01-07 15:54:47 +0800

    ErrorsCheckedExcep

--- src/main/java/org/example/exceptions/CheckedExceptions.java
+++ src/main/java/org/example/exceptions/CheckedExceptions.java
@@ -1,9 +1,14 @@
 package org.example.exceptions;
 
+import java.io.FileReader;
+import java.io.IOException;
+
 public class CheckedExceptions {
 
-    public void throwsFileNotFoundTest() {
-
+    public void throwsFileNotFoundTest() throws IOException {
+        String filePath = " ";
+        FileReader reader = new FileReader(filePath);
+        reader.read();
     }
 
 }
--- src/main/java/org/example/exceptions/Errors.java
+++ src/main/java/org/example/exceptions/Errors.java
@@ -1,10 +1,23 @@
 package org.example.exceptions;
 
+import java.util.LinkedList;
+
 public class Errors {
 
+    static LinkedList<Object[]> listObj = new LinkedList<>();
+
     public void throwsOOOException(){
+        while (true)
+        {
+            listObj.add(new Object[100]);
+        }
     }
 
     public void throwsStackOverFlowError(){
+        long x = nFactorial(1000);
+    }
+
+    private long nFactorial(int n){
+        return n * nFactorial(n-1);
     }
 }
