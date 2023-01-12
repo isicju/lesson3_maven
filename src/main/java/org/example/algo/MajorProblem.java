d7d8bb7bfdd48f3ec90ded494ad8c62727589f88
exenoyon <exenoyon@yandex.ru>
2023-01-13 00:52:55 +0800

    MajorProblemTest

--- src/main/java/org/example/algo/MajorProblem.java
+++ src/main/java/org/example/algo/MajorProblem.java
@@ -1,5 +1,9 @@
 package org.example.algo;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class MajorProblem {
 // Для заданного массива вернуть число которое встречается чаще всех остальных
 //Input : [2, 8, 7, 2, 2, 5, 2, 3, 1, 2, 2]
@@ -7,9 +11,40 @@ public class MajorProblem {
 //
 //Input : [1, 3, 1, 1]
 //Output: 1
+    public int findMajorElement(int[] arr){
 
+        if(arr == null)
+            throw new NullPointerException("this is not array");
+
+        if(arr.length == 0)
+            throw new ArrayIndexOutOfBoundsException("arr.length == 0");
+
+        return findMajorityElement(arr);
+    }
     private int findMajorityElement(int[] input){
-        return 0;
+        int maxi = -1;
+        Integer maxVal =0;
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i=0;i<input.length;++i) {
+            Integer temp = map.get(input[i]);
+            if(temp==null)
+            {
+               map.put(input[i],1);
+               if(maxVal < 1){
+                   maxVal = 1;
+                   maxi = input[i];
+               }
+            }
+            else {
+                map.put(input[i],temp+1);
+                if(maxVal < temp+1){
+                    maxVal = temp+1;
+                    maxi = input[i];
+                }
+            }
+
+        }
+        return maxi;
     }
 
 }
--- src/test/java/org/example/algo/MajorProblemTest.java
+++ src/test/java/org/example/algo/MajorProblemTest.java
@@ -1,9 +1,38 @@
 package org.example.algo;
 
 import org.example.DefaultTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MajorProblemTest extends DefaultTest {
 
-    //cover all main scenarios
+    @Test
+    void exceptionTesting()
+    {
+        MajorProblem majorProblem = new MajorProblem();
+        int[] x = null;
+        int[] y = {};
+        int[] z = {2, 8, 7, 2, 2, 5, 2, 3, 1, 2, 2};
+        int[] v = {1, 3, 1, 1};
+
+        assertThrows(
+                NullPointerException.class,
+                ()->majorProblem.findMajorElement(x),
+                "nullpointerexception"
+        );
+
+        assertThrows(
+                ArrayIndexOutOfBoundsException.class,
+                ()->majorProblem.findMajorElement(y),
+                "ArrayIndexOutOfBoundsException"
+        );
+
+        assertEquals(2, majorProblem.findMajorElement(z));
+        assertEquals(1, majorProblem.findMajorElement(v));
+    }
 
 }
